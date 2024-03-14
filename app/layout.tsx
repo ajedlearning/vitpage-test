4a6f6e113c72bc99267576e03215d1828178dfc2
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-03-14 11:06:42 -0400

    this commit implement Carousel with tw-element library

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        {children}
+        <main className="min-h-screen">
+          {children}
+        </main>
+
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Carousel from "@/app/components/Carousel";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          Bienvenidos a la Página de vit 
-    </main>
+    <>
+
+      
+      <Carousel />
+    </>
+
   );
 }
