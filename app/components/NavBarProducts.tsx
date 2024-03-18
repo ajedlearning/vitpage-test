68e3cccbe4de5854b8de0b89d7cebb5d9cfd5b29
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-03-18 08:57:42 -0400

    this commit improve the components NavbarProducts, ProductDetails and Products

--- app/components/NavBarProducts.tsx
+++ app/components/NavBarProducts.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function NavBarProducts() {
   return (
-    <nav className='flex justify-between w-[50%] mx-auto p-8 font-bold'>
-        <Link href={``} className='hover:bg-slate-400 p-4'>ESCRITORIO</Link>
-        <Link href={``} className='hover:bg-slate-400 p-4'>PORTATILES</Link>
-        <Link href={``} className='hover:bg-slate-400 p-4'>MINI COMPUTADOR</Link>
-        <Link href={``} className='hover:bg-slate-400 p-4'>TABLETAS</Link>
-        <Link href={``} className='hover:bg-slate-400 p-4'>SERVIDORES</Link>
+    <nav className='flex flex-col text-sm md:flex-row justify-between md:w-[80%] w-[50%] md:mx-auto p-6 font-bold'>
+        <Link href={``} className='hover:border-black rounded-md p-2 border border-transparent'>ESCRITORIO</Link>
+        <Link href={``} className='hover:border-black rounded-md p-2 border border-transparent'>PORTATILES</Link>
+        <Link href={``} className='hover:border-black rounded-md p-2 border border-transparent'>MINI COMPUTADOR</Link>
+        <Link href={``} className='hover:border-black rounded-md p-2 border border-transparent'>TABLETAS</Link>
+        <Link href={``} className='hover:border-black rounded-md p-2 border border-transparent'>SERVIDORES</Link>
     </nav>
   )
 }
--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -5,7 +5,7 @@ import ProductDetails from './ProductDetails'
 const Products = () => {
     return (
         <div className='w-full pt-10 '>
-                <h2 className="mb-6 text-2xl md:text-4xl lg:text-5xl font-bold p-2 text-center ">PRODUCTOS</h2>
+                <h2 className="mb-0 md:mb-6 text-2xl md:text-4xl lg:text-5xl font-bold p-2 text-center ">PRODUCTOS</h2>
 
             <NavBarProducts />
             <ProductDetails/>
