b5c786ed2ec63df0f819ce306b5e7e32a376331f
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-03-21 12:18:41 -0400

    this commit changes the font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/app/components/NavBar";
 import Footer from "@/app/components/Footer";
+import { inter } from '@/app/fonts';
 
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} antialiased`}>
         <NavBar />
         <main className="min-h-screen">
           {children}
