ce02ee5b9cdaced8f15f499f4d77dc209759c652
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-03-22 16:31:36 -0400

    this commit add usePathname in navBarProducto

--- app/components/NavBarProducts2.tsx
+++ app/components/NavBarProducts2.tsx
@@ -1,5 +1,9 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import {clsx} from 'clsx'
+import {usePathname} from 'next/navigation'
+
 const products = [
     {
       id: '1',
@@ -33,10 +37,11 @@ const products = [
     },
   ] 
 const NavBarProducts2 = () => {
+  const pathname = usePathname();
   return (
     <nav className='flex flex-col text-sm md:flex-row justify-between md:w-[80%] w-[70%] md:mx-auto p-4 md:p-6 font-bold'>
     {products.map((prod) => (
-      <Link href={prod.link}  id={prod.id} key={prod.id} className={`hover:border-black rounded-md p-2 border border-transparent`}>{prod.name}</Link>
+      <Link href={prod.link}  id={prod.id} key={prod.id} className={clsx('hover:border-black rounded-md p-2 border border-transparent', {'text-blue-600': pathname===prod.link})}>{prod.name}</Link>
     ))}
 
   </nav>
