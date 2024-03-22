f1d7ce47fc4381f60c8e69c3a140fa2f7a0c33ca
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-03-21 20:09:47 -0400

    fixed error in LinkNavaBAr

--- app/components/LinkNavbars.tsx
+++ app/components/LinkNavbars.tsx
@@ -45,7 +45,7 @@ export const LinkNavbar = () => {
 
         <Link
           href={ln.href}
-          className={clsx('hover:text-blue-600 text-gray-500 rounded-md px-3 py-2 text-sm font-medium', { 'text-black': scroll, 'text-gray-400': !scroll, 'text-blue-600': pathname === ln.href })}
+          className={clsx('hover:text-blue-600 text-gray-500 rounded-md px-3 py-2 text-sm font-medium', {'text-blue-600': pathname === ln.href })}
           aria-current="page"
           key={ln.name}
         >
